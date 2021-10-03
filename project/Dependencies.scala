0a462d010578054fe62fb74f53e3511907a4cc5c
Scala Steward <me@scala-steward.org>
2021-10-03 04:08:15 +0200

    Update pureconfig to 0.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     "org.tpolecat" %% "doobie-core" % doobieVersion,
     "org.tpolecat" %% "doobie-hikari" % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest" % doobieVersion % Test,
-    "com.github.pureconfig" %% "pureconfig" % "0.16.0",
+    "com.github.pureconfig" %% "pureconfig" % "0.17.0",
     "mysql" % "mysql-connector-java" % "8.0.25",
     "org.scalatest" %% "scalatest" % "3.2.9" % Test,
     "com.monovore" %% "decline" % "2.1.0",
