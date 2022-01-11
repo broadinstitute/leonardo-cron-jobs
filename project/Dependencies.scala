6364263571f48e855e53b3413c800cca2c11d0c9
Qi Wang <32771737+Qi77Qi@users.noreply.github.com>
2022-01-11 09:57:59 -0500

    bump wblibs (#175)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val workbenchGoogle2Version = "0.23-fdc25f3"
+  val workbenchGoogle2Version = "0.23-3b927f8"
   val doobieVersion = "1.0.0-RC1"
   val openTelemetryVersion = "0.2-03a7abb"
   val declineVersion = "2.2.0"
