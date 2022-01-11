e59d023b302e52a8c59c2224b7e42913eb2d25d5
Scala Steward <me@scala-steward.org>
2022-01-11 22:16:18 +0100

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val workbenchGoogle2Version = "0.23-3b927f8"
-  val doobieVersion = "1.0.0-RC1"
+  val doobieVersion = "1.0.0-RC2"
   val openTelemetryVersion = "0.2-03a7abb"
   val declineVersion = "2.2.0"
 
