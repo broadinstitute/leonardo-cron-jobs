09152f1d4caed0c6cd2a2599cb06bcb30b9c6203
Scala Steward <me@scala-steward.org>
2022-02-04 14:09:50 +0100

    Update workbench-opentelemetry to 0.3-ea061ce

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val workbenchGoogle2Version = "0.23-3b927f8"
   val doobieVersion = "1.0.0-RC1"
-  val openTelemetryVersion = "0.2-03a7abb"
+  val openTelemetryVersion = "0.3-ea061ce"
   val declineVersion = "2.2.0"
 
   val core = Seq(
