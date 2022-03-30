fb4ec84b4a0e14aab7b8303ddaabf3ed583967a6
Qi Wang <qi@broadinstitute.org>
2022-03-29 21:13:59 -0400

    fix formatting

--- core/src/test/scala/com/broadinstitute/dsp/Generators.scala
+++ core/src/test/scala/com/broadinstitute/dsp/Generators.scala
@@ -33,7 +33,12 @@ object Generators {
     project <- genGoogleProject
     diskName <- genDiskName
     zone <- genZoneName
-  } yield Disk(id, CloudContext.Gcp(project), diskName, zone, formattedBy = Some("GCE")) // TODO: update generator once we support Azure
+  } yield Disk(id,
+               CloudContext.Gcp(project),
+               diskName,
+               zone,
+               formattedBy = Some("GCE")
+  ) // TODO: update generator once we support Azure
 
   val genInitBucket: Gen[InitBucketToRemove] = for {
     project <- genGoogleProject
