81c6b1591d46fa834c7baff939811ecc8973e587
Liz Baldo <baldo@wm26e-4a3.broadinstitute.org>
2023-01-10 16:07:12 -0500

    forgot to propagate to main

--- janitor/src/main/scala/com/broadinstitute/dsp/janitor/StuckAppReporter.scala
+++ janitor/src/main/scala/com/broadinstitute/dsp/janitor/StuckAppReporter.scala
@@ -3,11 +3,10 @@ package janitor
 
 import cats.effect.Concurrent
 import cats.mtl.Ask
-import cats.syntax.all._
 import org.broadinstitute.dsde.workbench.model.TraceId
 import org.typelevel.log4cats.Logger
 
-class StuckAppReporter {
+object StuckAppReporter {
   def impl[F[_]](
     dbReader: DbReader[F],
     deps: LeoPublisherDeps[F]
