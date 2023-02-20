96cdf21a08915a915650b176af870648aee666a6
broadbot <broadbot@broadinstitute.org>
2023-02-20 12:01:40 +0000

    Update logstash-logback-encoder to 7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     ExclusionRule(organization = "com.azure.resourcemanager", name = s"azure-resourcemanager-msi")
 
   val core = Seq(
-    "net.logstash.logback" % "logstash-logback-encoder" % "7.2",
+    "net.logstash.logback" % "logstash-logback-encoder" % "7.3",
     "ch.qos.logback" % "logback-classic" % "1.4.5",
     "ch.qos.logback" % "logback-core" % "1.4.5",
     "org.tpolecat" %% "doobie-core" % doobieVersion,
