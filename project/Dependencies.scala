0a1bcb67bb87e3ab21cd468f6210995ec1b3d3b0
Qi Wang <qi@broadinstitute.org>
2024-01-26 14:46:48 -0500

    dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,6 +22,9 @@ object Dependencies {
   val excludeResourceManagerMsi =
     ExclusionRule(organization = "com.azure.resourcemanager", name = s"azure-resourcemanager-msi")
 
+  val googleCloudNio: ModuleID =
+    "com.google.cloud" % "google-cloud-nio" % "0.127.9" % Test // brought in for FakeStorageInterpreter
+
   val core = Seq(
     "net.logstash.logback" % "logstash-logback-encoder" % "7.4",
     "ch.qos.logback" % "logback-classic" % "1.4.11",
@@ -52,7 +55,7 @@ object Dependencies {
     "org.scalatestplus" %% "scalacheck-1-16" % "3.2.14.0" % Test,
     "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % Test, // https://github.com/scalatest/scalatestplus-selenium
     "ca.mrvisser" %% "sealerate" % "0.0.6",
-    "com.google.cloud" % "google-cloud-nio" % "0.127.9" % Test classifier "tests"
+    googleCloudNio
   )
 
   val resourceValidator =
