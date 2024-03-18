9774cbee2ab7016fe0dfc33e58fe4b741f7a43b0
Qi Wang <32771737+Qi77Qi@users.noreply.github.com>
2024-03-18 09:44:48 -0400

    look for runtimes without destroyedDate as well (#328)

--- resource-validator/src/main/scala/com/broadinstitute/dsp/resourceValidator/DbReader.scala
+++ resource-validator/src/main/scala/com/broadinstitute/dsp/resourceValidator/DbReader.scala
@@ -50,8 +50,11 @@ object DbReader {
           FROM CLUSTER AS c1
           INNER JOIN RUNTIME_CONFIG AS rt ON c1.runtimeConfigId = rt.id
           WHERE
-            c1.status = "Deleted" AND
-            c1.destroyedDate > now() - INTERVAL 30 DAY AND
+            (
+              (c1.status = "Deleted" AND c1.destroyedDate > now() - INTERVAL 30 DAY) OR
+              (c1.status="Deleted" AND c1.destroyedDate = '1970-01-01 00:00:01.000000')
+            )
+            AND
             NOT EXISTS (
               SELECT *
               FROM CLUSTER AS c2
