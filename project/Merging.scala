9c47fd5149635d0cec309ba2d599f529278437eb
LizBaldo <baldo@broadinstitute.org>
2024-03-27 13:30:34 -0400

    try discarding

--- project/Merging.scala
+++ project/Merging.scala
@@ -5,8 +5,8 @@ object Merging {
     case PathList("META-INF", "versions", "9", _ @_*)         => MergeStrategy.discard
     case PathList("META-INF", "versions", "11", _ @_*)        => MergeStrategy.discard
     case PathList("META-INF", "io.netty.versions.properties") => MergeStrategy.concat
-    case PathList("META-INF", "com.squareup.okio")            => MergeStrategy.concat
     case PathList("module-info.class")                        => MergeStrategy.concat
+    case PathList("META-INF", "com.squareup.okio")            => MergeStrategy.discard
     case "reference.conf"                                     => MergeStrategy.concat
     case x =>
       oldStrategy(x)
